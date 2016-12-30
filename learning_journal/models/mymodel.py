0d70a66df8bdafa7a82553facbda0b72d2992f34
Your Name <you@example.com>
2016-12-29 17:22:24 -0800

    mymodel and views/default

--- learning_journal/models/mymodel.py
+++ learning_journal/models/mymodel.py
@@ -3,16 +3,27 @@ from sqlalchemy import (
     Index,
     Integer,
     Unicode,
+    Date,
 )
 
 from .meta import Base
 
+class Expense(Base):
+    """ expense object."""
+    __tablename__= "expenses"
+    id = Coumn(Integer, primary)
+    item= Column(Unicode)
+
 
 class MyModel(Base):
+    """."""
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
     name = Column(Unicode)
     value = Column(Integer)
+    category = Column(Unicode)
+    date = Column(Date)
+    description = Column(Unicode)
 
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
