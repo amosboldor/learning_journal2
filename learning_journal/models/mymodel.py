ec8a1af4e027d06290ab7c8dc9389c00310f6cd3
Your Name <you@example.com>
2016-12-30 01:02:58 -0800

    models.py

--- learning_journal/models/mymodel.py
+++ learning_journal/models/mymodel.py
@@ -13,24 +13,11 @@ from .meta import Base
 
 
 class Entries(Base):
-    """Entries object."""
+    """Entries object."""\
 
     __tablename__ = "entries"
     id = Column(Integer, primary_key=True)
     title = Column(Unicode)
+    title1 = Column(Unicode)
     create_date = Column(Date)
     body = Column(Unicode)
-
-
-# class MyModel(Base):
-#     """."""
-#     __tablename__ = 'models'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(Unicode)
-#     value = Column(Integer)
-#     category = Column(Unicode)
-#     date = Column(Date)
-#     description = Column(Unicode)
-
-
-# Index('my_index', MyModel.name, unique=True, mysql_length=255)
