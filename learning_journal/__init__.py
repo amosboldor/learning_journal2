247ac7c9a4d53bd2ed065592a18ab42cf4727d6b
Your Name <you@example.com>
2017-01-09 07:04:31 -0800

    userstory ajax

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -1,19 +1,11 @@
 """."""
 
-<<<<<<< HEAD
-=======
 
-import os
->>>>>>> b7be6d832a98759f9a0f9ce4e34b32ab9f1db0d2
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
-<<<<<<< HEAD
-=======
-    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
->>>>>>> b7be6d832a98759f9a0f9ce4e34b32ab9f1db0d2
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
