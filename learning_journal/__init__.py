aff858781270f49f5f7b1ba05733654eaf33e8b9
Your Name <you@example.com>
2017-01-09 05:42:07 -0800

    bin/init

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -1,13 +1,13 @@
 """."""
 
 
-import os
+# import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
-    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+    # settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
